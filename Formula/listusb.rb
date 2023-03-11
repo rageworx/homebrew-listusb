232f7c75c737026936fa99793b129587cbdb4644
Rapahael Kim <rageworx@gmail.com>
2023-03-11 21:56:26 +0900

    updating Formula description

--- Formula/listusb.rb
+++ Formula/listusb.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Listusb < Formula
-    desc "A tool for list usb devices for macOS universal"
+    desc "A tool for list USB devices for macOS universal mach-O binary"
     homepage "https://github.com/rageworx/listusb"
     url "https://github.com/rageworx/listusb/releases/download/v0.2.0.10/listusb-macOS11-ub-v0.2.0.10.tar.gz"
     sha256 "8722d0c0a728341f0f6a922e94220e5e99a94c94635bdc8991d263627954fe6e"
